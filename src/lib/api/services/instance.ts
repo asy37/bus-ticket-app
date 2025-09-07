2a6e6bdc46cb4a19041a3423045d920ed192a788
asy37 <ahmetsylmz37@gmail.com>
2025-09-08 01:50:19 +0300

    feat: env

--- src/lib/api/services/instance.ts
+++ src/lib/api/services/instance.ts
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-const CLIENT_API_URL = 'http://localhost:3000';
-
 const base = axios.create({
-  baseURL: CLIENT_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
 export const Instance = {
   base,
 };
